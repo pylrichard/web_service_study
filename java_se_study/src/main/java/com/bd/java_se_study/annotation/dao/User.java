581b3fb6bfc22c53eec42e872499f1ff51874404
pylrichard <pylrichard@qq.com>
2017-09-26 23:58:48 +0800

    java_se_study-添加注解应用例子，实现DAO

---
+++ java_se_study/src/main/java/com/bd/java_se_study/annotation/dao/User.java
@@ -0,0 +1,48 @@
+package com.bd.java_se_study.annotation.dao;
+
+/**
+ * 将类映射到数据库表
+ */
+@Table("user")
+public class User {
+    @Column("id")
+    private int id;
+    @Column("user_name")
+    private String userName;
+    @Column("age")
+    private int age;
+    @Column("email")
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
