350d0635f3d7410db040e081fa73153531e7c7c2
pylrichard <pylrichard@qq.com>
2017-10-11 20:53:17 +0800

    roncoo_spring_boot-集成Druid

--- roncoo_spring_boot/src/main/java/com/roncoo/spring_boot/RoncooSpringBootApplication.java
+++ roncoo_spring_boot/src/main/java/com/roncoo/spring_boot/RoncooSpringBootApplication.java
@@ -2,12 +2,17 @@ package com.roncoo.spring_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
+//让Spring扫描到Servlet和Filter
+@ServletComponentScan
 @EnableCaching
 @EnableJms
+@ImportResource(locations = { "classpath:druid-bean.xml" })
 public class RoncooSpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(RoncooSpringBootApplication.class, args);
