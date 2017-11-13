efb6921f81acf9ee70c6c468cce0e9a4ee73c274
pylrichard <pylrichard@qq.com>
2017-11-13 13:52:03 +0800

    roncoo_book_shop-10-Repository-分页和排序

--- roncoo_book_shop/shop_db/src/main/java/com/bd/roncoo/book/shop/db/repository/BookRepository.java
+++ roncoo_book_shop/shop_db/src/main/java/com/bd/roncoo/book/shop/db/repository/BookRepository.java
@@ -1,13 +1,13 @@
 package com.bd.roncoo.book.shop.db.repository;
 
 import com.bd.roncoo.book.shop.db.domain.Book;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 /**
  * Repository(T, ID)中泛型T代表操作的数据库表，ID代表主键类型
  */
-public interface BookRepository extends CrudRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findByName(String name);
 }
--- roncoo_book_shop/shop_db/src/test/java/com/bd/roncoo/book/shop/db/repository/RepositoryTest.java
+++ roncoo_book_shop/shop_db/src/test/java/com/bd/roncoo/book/shop/db/repository/RepositoryTest.java
@@ -4,6 +4,7 @@ import com.bd.roncoo.book.shop.db.BaseTest;
 import com.bd.roncoo.book.shop.db.domain.Book;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +17,9 @@ public class RepositoryTest extends BaseTest {
     @Autowired
     private BookRepository bookRepository;
 
+    /**
+     * BookRepository继承CrudRepository
+     */
     @Test
     public void testFindByName() {
         System.out.println(bookRepository.getClass().getName());
@@ -45,7 +49,7 @@ public class RepositoryTest extends BaseTest {
     }
 
     @Test
-    public void testFindAll() {
+    public void testFindAll1() {
         List<Long> ids = new ArrayList<>();
         ids.add(1L);
         ids.add(2L);
@@ -57,5 +61,55 @@ public class RepositoryTest extends BaseTest {
     @Test
     public void testCount() {
         System.out.println(bookRepository.count());
+    }
+
+    /**
+     * BookRepository继承PagingAndSortingRepository
+     */
+    @Test
+    public void testFindAll2() {
+        /*
+            SQL语句包含order by子句
+            bookRepository.findAll(new Sort(Direction.DESC, "name", "id"));
+            按name降序排，按id升序排，Order(String property)中DEFAULT_DIRECTION默认是Direction.ASC，可以不写
+        */
+        bookRepository.findAll(new Sort(new Sort.Order(Sort.Direction.DESC, "name"), new Sort.Order("id")));
+    }
+
+    @Test
+    public void testFindAll3() {
+        /*
+            Open Type Hierarchy查看继承树，使用实现类PageRequest创建对象
+            从第1页开始，每页取10条记录
+        */
+        Pageable req = new PageRequest(0, 10, new Sort(Sort.Direction.DESC, "name"));
+        //返回值Page<Book> books的getContent()返回List<Book>，比如表中共有15条记录，每页取10条记录，查第1页List有10条，查第2页List有5条
+        bookRepository.findAll(req);
+    }
+
+    /**
+     * BookRepository继承JpaRepository
+     */
+    @Test
+    public void testFindAll4() {
+        Pageable req = new PageRequest(0, 10, new Sort(Sort.Direction.DESC, "name"));
+        Book book = new Book();
+        book.setName("战争与和平");
+        Example<Book> example = Example.of(book);
+        bookRepository.findAll(example, req);
+    }
+
+    @Test
+    public void testFindAll5() {
+        Pageable req = new PageRequest(0, 10, new Sort(Sort.Direction.DESC, "name"));
+        Book book = new Book();
+        book.setName("战争");
+        /*
+            生成的SQL语句包含like
+            不支持group by，or，日期大于小于，between
+        */
+        ExampleMatcher matcher = ExampleMatcher.matching().withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+        Example<Book> example = Example.of(book, matcher);
+        bookRepository.findAll(example, req);
     }
 }
