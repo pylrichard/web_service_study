8ee0bb817a96471f48d06e3ca1939575df9103d7
pylrichard <pylrichard@qq.com>
2017-11-16 18:10:53 +0800

    roncoo_book_shop-21-REST API开发-概述

---
+++ roncoo_book_shop/shop_admin/src/main/java/com/bd/roncoo/book/shop/admin/AdminApplication.java
@@ -0,0 +1,11 @@
+package com.bd.roncoo.book.shop.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
