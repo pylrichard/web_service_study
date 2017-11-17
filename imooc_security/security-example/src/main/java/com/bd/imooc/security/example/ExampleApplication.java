f984586cea37be1de35806d183c5a23487f4d017
pylrichard <pylrichard@qq.com>
2017-11-17 11:58:57 +0800

    imooc_security-2.3-Hello Spring Security

---
+++ imooc_security/security-example/src/main/java/com/bd/imooc/security/example/ExampleApplication.java
@@ -0,0 +1,19 @@
+package com.bd.imooc.security.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello spring security";
+    }
+}
