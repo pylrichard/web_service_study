7e63641a5619635e162cd9d5c309714857bfab45
pylrichard <pylrichard@qq.com>
2017-11-17 13:33:23 +0800

    imooc_security-3.2-查询请求

---
+++ imooc_security/security-example/src/main/java/com/bd/imooc/security/example/dto/User.java
@@ -0,0 +1,42 @@
+package com.bd.imooc.security.example.dto;
+
+import java.util.Date;
+
+public class User {
+    private String id;
+    private String username;
+    private String password;
+    private Date birthday;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}
---
+++ imooc_security/security-example/src/test/java/com/bd/imooc/security/example/web/controller/UserControllerTest.java
@@ -0,0 +1,56 @@
+package com.bd.imooc.security.example.web.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserControllerTest {
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    public void whenQuerySuccess() throws Exception {
+        String result = mockMvc.perform(
+                /*
+                    设置GET请求参数，组装为UserQueryCondition
+                 */
+                get("/user")
+                        .param("username", "pyl").param("age", "18")
+                        .param("ageTo", "60").param("xxx", "yyy")
+                        //.param("size", "15")
+                        //.param("page", "3")
+                        //.param("sort", "age,desc")
+                        //设置请求类型
+                        .contentType(MediaType.APPLICATION_JSON_UTF8))
+                /*
+                    设置预期结果
+                 */
+                //设置预期返回状态码
+                .andExpect(status().isOk())
+                //设置预期返回值长度，$等符号意义见JsonPath Github
+                .andExpect(jsonPath("$.length()").value(3))
+                .andReturn().getResponse().getContentAsString();
+
+        System.out.println(result);
+    }
+}
