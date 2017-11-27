0f6dd817295a42814764123903c2447d9fece665
pylrichard <pylrichard@qq.com>
2017-11-27 17:51:48 +0800

    imooc_security-6.4-重构用户名密码登录

--- imooc_security/security_app/src/main/java/com/bd/imooc/security/app/ImoocResourceServerConfig.java
+++ imooc_security/security_app/src/main/java/com/bd/imooc/security/app/ImoocResourceServerConfig.java
@@ -1,11 +1,37 @@
 package com.bd.imooc.security.app;
 
+import com.bd.imooc.security.core.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
+import com.bd.imooc.security.core.validate.code.ValidateCodeSecurityConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.social.security.SpringSocialConfigurer;
 
 /**
  * Example作为资源服务器
  */
 @Configuration
 @EnableResourceServer
-public class ImoocResourceServerConfig {}
+public class ImoocResourceServerConfig extends ResourceServerConfigurerAdapter {
+    @Autowired
+    private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
+
+    @Autowired
+    private ValidateCodeSecurityConfig validateCodeSecurityConfig;
+
+    @Autowired
+    private SpringSocialConfigurer imoocSocialSecurityConfig;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.apply(validateCodeSecurityConfig)
+            .and()
+            .apply(smsCodeAuthenticationSecurityConfig)
+            .and()
+            .apply(imoocSocialSecurityConfig)
+            .and()
+            .csrf().disable();
+    }
+}
--- imooc_security/security_app/src/main/java/com/bd/imooc/security/app/authenctiation/ImoocAuthenticationSuccessHandler.java
+++ imooc_security/security_app/src/main/java/com/bd/imooc/security/app/authenctiation/ImoocAuthenticationSuccessHandler.java
@@ -1,12 +1,18 @@
 package com.bd.imooc.security.app.authenctiation;
 
-import com.bd.imooc.security.core.properties.SecurityProperties;
-import com.bd.imooc.security.core.properties.SignInResponseType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.UnapprovedClientAuthenticationException;
+import org.springframework.security.oauth2.provider.*;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -23,18 +29,64 @@ public class ImoocAuthenticationSuccessHandler extends SavedRequestAwareAuthenti
     private ObjectMapper objectMapper;
 
     @Autowired
-    private SecurityProperties securityProperties;
+    private ClientDetailsService clientDetailsService;
+
+    @Autowired
+    private AuthorizationServerTokenServices authorizationServerTokenServices;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
         logger.info("登录成功");
 
-        if (SignInResponseType.JSON.equals(securityProperties.getBrowser().getSignInResponseType())) {
-            response.setContentType("application/json;charset=UTF-8");
-            response.getWriter().write(objectMapper.writeValueAsString(authentication));
-        } else {
-            super.onAuthenticationSuccess(request, response, authentication);
+        String header = request.getHeader("Authorization");
+        if (header == null || !header.startsWith("Basic ")) {
+            throw new UnapprovedClientAuthenticationException("请求头中无client信息");
         }
+        String[] tokens = extractAndDecodeHeader(header, request);
+        assert tokens.length == 2;
+        String clientId = tokens[0];
+        String clientSecret = tokens[1];
+        //获取ClientDetails
+        ClientDetails clientDetails = clientDetailsService.loadClientByClientId(clientId);
+        if (clientDetails == null) {
+            throw new UnapprovedClientAuthenticationException("clientId对应的配置信息不存在:" + clientId);
+        } else if (!StringUtils.equals(clientDetails.getClientSecret(), clientSecret)) {
+            throw new UnapprovedClientAuthenticationException("clientSecret不匹配:" + clientId);
+        }
+        /*
+            不同授权模式处理逻辑相同，传入的参数不同
+
+            第一个参数是授权模式特有参数用来创建Authentication(登录流程已创建)，此处不需要，设置为空Map
+         */
+        TokenRequest tokenRequest = new TokenRequest(MapUtils.EMPTY_MAP, clientId,
+                                                    clientDetails.getScope(), "custom");
+        OAuth2Request oAuth2Request = tokenRequest.createOAuth2Request(clientDetails);
+        OAuth2Authentication oAuth2Authentication = new OAuth2Authentication(oAuth2Request, authentication);
+        //获取授权令牌
+        OAuth2AccessToken token = authorizationServerTokenServices.createAccessToken(oAuth2Authentication);
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().write(objectMapper.writeValueAsString(token));
+    }
+
+    private String[] extractAndDecodeHeader(String header, HttpServletRequest request) throws IOException {
+        //取出头部的Basic
+        byte[] base64Token = header.substring(6).getBytes("UTF-8");
+        byte[] decoded;
+        try {
+            //解析得到clientId和clientSecret
+            decoded = Base64.decode(base64Token);
+        } catch (IllegalArgumentException e) {
+            throw new BadCredentialsException("Failed to decode basic authentication token");
+        }
+        String token = new String(decoded, "UTF-8");
+        //获取clientId和clientSecret冒号分隔符
+        int delim = token.indexOf(":");
+        if (delim == -1) {
+            throw new BadCredentialsException("Invalid basic authentication token");
+        }
+
+        //冒号之前是clientId，之后是clientSecret
+        return new String[]{token.substring(0, delim), token.substring(delim + 1)};
     }
 }
