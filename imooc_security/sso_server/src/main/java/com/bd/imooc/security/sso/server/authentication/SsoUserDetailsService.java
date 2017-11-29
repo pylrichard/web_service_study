bb3c8f80495cba325c83339a69d0768ab8defec1
pylrichard <pylrichard@qq.com>
2017-11-29 11:44:28 +0800

    imooc_security-6.11-基于JWT实现SSO单点登录

---
+++ imooc_security/sso_server/src/main/java/com/bd/imooc/security/sso/server/authentication/SsoUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.bd.imooc.security.sso.server.authentication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SsoUserDetailsService implements UserDetailsService {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new User(username, passwordEncoder.encode("Pyl123456"),
+                AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"));
+    }
+}
--- imooc_security/sso_server/src/main/java/com/bd/imooc/security/sso/server/config/SsoAuthorizationServerConfig.java
+++ imooc_security/sso_server/src/main/java/com/bd/imooc/security/sso/server/config/SsoAuthorizationServerConfig.java
@@ -1,4 +1,4 @@
-package com.bd.imooc.security.sso.server;
+package com.bd.imooc.security.sso.server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
