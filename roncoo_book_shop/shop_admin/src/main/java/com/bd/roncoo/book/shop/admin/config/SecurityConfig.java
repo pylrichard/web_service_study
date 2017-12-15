207ad58c8d4f2b7b025aead634f6f567a203620e
pylrichard <pylrichard@qq.com>
2017-12-15 10:43:02 +0800

    roncoo_book_shop-33-REST API安全-Spring Security概述和基本配置

---
+++ roncoo_book_shop/shop_admin/src/main/java/com/bd/roncoo/book/shop/admin/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.bd.roncoo.book.shop.admin.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //以表单的形式验证登录
+        http.formLogin()
+                .and()
+                //验证请求，/book请求所有用户都可以访问
+                .authorizeRequests().antMatchers("/book").permitAll()
+                //其它请求都需要验证
+                .anyRequest().authenticated();
+    }
+}
