f22905bdbc7e37ce772474ec89b1bf29f34767d5
pylrichard <pylrichard@qq.com>
2017-12-18 19:15:22 +0800

    roncoo_book_shop-55-分布式服务开发-事务控制

--- roncoo_book_shop/shop_book_service/src/main/java/com/bd/roncoo/book/shop/book/service/impl/BookServiceImpl.java
+++ roncoo_book_shop/shop_book_service/src/main/java/com/bd/roncoo/book/shop/book/service/impl/BookServiceImpl.java
@@ -9,24 +9,38 @@ import com.bd.roncoo.book.shop.db.repository.BookRepository;
 import com.bd.roncoo.book.shop.db.repository.specification.BookSpecification;
 import com.bd.roncoo.book.shop.db.repository.support.AbstractDomain2InfoConverter;
 import com.bd.roncoo.book.shop.db.repository.support.QueryResultConverter;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 @Service("bookService")
+//@Transactional注解的类的public方法在一个事务中执行
 @Transactional(rollbackFor = Exception.class)
 public class BookServiceImpl implements BookService {
     @Autowired
     private BookRepository bookRepository;
+
+    @Autowired
+    private PlatformTransactionManager transactionManager;
 
     @Override
     public BookInfo getInfo(Long id) {
         Book book = bookRepository.findOne(id);
         BookInfo bookInfo = new BookInfo();
         BeanUtils.copyProperties(book, bookInfo);
+
+        /*
+            在同一个类中调用query()，注解@Transactional不起作用
+         */
+//		query(new BookCondition(), new PageRequest(0, 20));
 
         return bookInfo;
     }
@@ -38,6 +52,9 @@ public class BookServiceImpl implements BookService {
      * PageImpl是返回的一页数据
      * PageRequest是分页请求
      * Sort是分页排序策略
+     *
+     * 添加@Transactional后，抛出异常整个事务会回滚，不会插入记录
+     * 注解@Transactional要起作用，需要从外部类调用query()
      */
     @ServiceLog
     @Override
@@ -61,8 +78,40 @@ public class BookServiceImpl implements BookService {
         });
     }
 
+    @Transactional
     @Override
     public BookInfo create(BookInfo info) {
+        /*
+            通过代码进行事务控制
+         */
+        DefaultTransactionDefinition definition = new DefaultTransactionDefinition();
+        definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+        TransactionStatus status = transactionManager.getTransaction(definition);
+        /*
+            如果create()没有注解@Transactional，以下2条update语句分别在2个事务中执行
+         */
+        try {
+            update(new BookInfo("a"));
+        } catch (Exception e) {
+            transactionManager.rollback(status);
+        }
+        transactionManager.commit(status);
+        update(new BookInfo("b"));
+
+        /*
+            分布式事务控制
+            可靠消息补偿机制，优点是开发相对简单，缺点是保持数据最终一致性，如果消息队列数据量较大，数据会有一段时间是不正确的
+
+            下订单流程：
+            createOrder() 创建订单记录
+            updateStock() 更新库存
+            updateUserBalance() 更新用户余额
+            比如发现用户余额不够，会抛出异常，如果3次调用发生在单机，位于一个事务中，会进行回滚
+            3次调用的服务是分布式的，每个服务有自己的事务
+            updateUserBalance()所在服务会往消息队列中发送一条消息，表示某个订单扣除用户余额失败
+            updateStock()所在服务要监听这类消息，在数据库中查询相应订单是否扣除了库存，如果执行了扣除则进行回滚
+         */
+
         Book book = new Book();
         book.setName(info.getName());
         bookRepository.save(book);
@@ -73,6 +122,13 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public BookInfo update(BookInfo info) {
+        /*
+            抛出异常的事务会进行回滚
+         */
+        if (StringUtils.equals(info.getName(), "b")) {
+            throw new RuntimeException("transaction test");
+        }
+
         Book book = bookRepository.findOne(info.getId());
         book.setName(info.getName());
         bookRepository.save(book);
--- roncoo_book_shop/shop_db/src/main/java/com/bd/roncoo/book/shop/db/repository/BookShopRepositoryImpl.java
+++ roncoo_book_shop/shop_db/src/main/java/com/bd/roncoo/book/shop/db/repository/BookShopRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.bd.roncoo.book.shop.db.repository;
 
 import org.springframework.data.jpa.repository.support.JpaEntityInformation;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
@@ -14,6 +15,18 @@ public class BookShopRepositoryImpl<T> extends SimpleJpaRepository<T, Long> {
         super(entityInformation, entityManager);
     }
 
+    /**
+     * SimpleJpaRepository有注解@Transactional(readOnly = true)
+     * SimpleJpaRepository.save()单独声明注解@Transactional
+     * 此处覆盖save()，需要声明注解@Transactional
+     * <p>
+     * Propagation.REQUIRED 当前调用链没有事务，创建新事务，如果已经有事务，则将方法调用放入当前事务，不创建新事务
+     * Propagation.REQUIRES_NEW 无论当前调用链是否有事务，都创建新事务
+     * 比如订单处理中下订单，下订单方法一旦被调用，创建一条日志，之后进行数据库操作，如果操作成功，日志标为成功，如果操作失败，日志标为失败
+     * 无论操作成功/失败，日志创建必须成功，所以将日志创建放在一个新事务中
+     */
+    //@Transactional(propagation = Propagation.REQUIRES_NEW)
+    @Transactional(rollbackFor = Exception.class)
     @Override
     public <S extends T> S save(S entity) {
         System.out.println("saved: " + entity.getClass().getSimpleName());
