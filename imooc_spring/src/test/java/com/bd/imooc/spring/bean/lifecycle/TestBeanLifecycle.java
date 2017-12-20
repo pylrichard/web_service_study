f030c02e0e03de34487ea10ef689ce8a7f15f430
pylrichard <pylrichard@qq.com>
2017-12-20 11:16:46 +0800

    imooc_spring-3.2-Bean装配-Bean的生命周期

---
+++ imooc_spring/src/test/java/com/bd/imooc/spring/bean/lifecycle/TestBeanLifecycle.java
@@ -0,0 +1,18 @@
+package com.bd.imooc.spring.bean.lifecycle;
+
+import com.bd.imooc.spring.base.UnitTestBase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class TestBeanLifecycle extends UnitTestBase {
+    public TestBeanLifecycle() {
+        super("classpath:spring-bean-lifecycle.xml");
+    }
+
+    @Test
+    public void test() {
+        super.getBean("beanLifeCycle");
+    }
+}
